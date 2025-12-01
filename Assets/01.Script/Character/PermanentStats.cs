30c81b0212c8926ebce41460335e116823498dc8
EoH <zien33195@gmail.com>
2025-12-01 21:15:37 +0900

    feat: EvolutionNPC 뼈대

---
+++ Assets/01.Script/Character/PermanentStats.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class PermanentStats : MonoBehaviour
+{
+    public int maxHpLevel;
+    public int attackLevel;
+    public int defenseLevel;
+    public int moveSpeedLevel;
+
+    public void IncreaseMaxHealthLevel()
+    {
+        maxHpLevel++;
+    }
+
+    public void IncreaseAttackLevel()
+    {
+        attackLevel++;
+    }
+
+    public void IncreaseDefenseLevel()
+    {
+        defenseLevel++;
+    }
+
+    public void IncreaseSpeedLevel()
+    {
+        moveSpeedLevel++;
+    }
+}
--- Assets/01.Script/Enum/Enum.cs
+++ Assets/01.Script/Enum/Enum.cs
@@ -10,3 +10,11 @@ public enum ItemType
     Currency,   // 재화 (골드, 조각들)
     Equipment   // 장비 (외피 같은 거)
 }
+
+public enum EvolutionStatType
+{
+    MaxHP,
+    Attack,
+    Defense,
+    MoveSpeed
+}
