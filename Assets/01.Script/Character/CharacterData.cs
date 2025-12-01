f7422b8b40e6a02ea6acc9b85cfbdf8227921b4e
Lim <mlimsg08@gmail.com>
2025-12-01 15:47:40 +0900

    feat: input시스템 추가 및 스크립트 일부 작성

---
+++ Assets/01.Script/Character/CharacterData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public struct CharacterData
+{
+    public int ID;
+    public string Name;
+    public int ATK;
+    public int DEF;
+    public int SPEED;
+    public int HP;
+    public int Atk_Value;
+    public int Def_Value;
+    public int Spd_Value_min;
+    public int Spd_Value_max;
+
+
+
+}
