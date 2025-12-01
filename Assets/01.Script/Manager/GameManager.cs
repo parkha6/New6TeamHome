7c25b7db07268c32d5e5d5689ddbe8f715eb34c4
Park ha <115542242+parkha6@users.noreply.github.com>
2025-12-01 17:47:41 +0900

    feat 게임매니저 함수 추가

--- Assets/01.Script/Manager/GameManager.cs
+++ Assets/01.Script/Manager/GameManager.cs
@@ -1,4 +1,4 @@
-using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 /// <summary>
 /// 게임매니저 순서관리
@@ -11,5 +11,21 @@ public class GameManager : MonoSingleton<GameManager>
     /// <param name="sceneName"></param>
     internal void ChangeScene(string sceneName)
     { SceneManager.LoadScene(sceneName); }
-
+    /// <summary>
+    /// 일시정지시 UI띄우기
+    /// </summary>
+    internal void Pause()
+    { StopTime(); }
+    internal void EndPause()
+    { RunTime(); }
+    /// <summary>
+    /// 시간을 멈춘다.
+    /// </summary>
+    void StopTime()
+    { Time.timeScale = Consts.none; }
+    /// <summary>
+    /// 시간을 다시 움직인다.
+    /// </summary>
+    void RunTime()
+    { Time.timeScale = Consts.minValue; }
 }
