52097c30affd33fbad289b1f93d69a2985b030bd
EoH <zien33195@gmail.com>
2025-12-01 16:34:17 +0900

    feat: itemdata

--- Assets/01.Script/Enum/Enum.cs
+++ Assets/01.Script/Enum/Enum.cs
@@ -4,3 +4,9 @@
     Grasshopper, // 메뚜기
     Cockroach // 바퀴벌레
 }
+
+public enum ItemType
+{
+    Currency,   // 재화 (골드, 조각들)
+    Equipment   // 장비 (외피 같은 거)
+}
---
+++ Assets/01.Script/ScriptableObject/itemData.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Item")]
+public class itemData : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public Sprite icon;
+
+    public ItemType itemType;    // Currency / Equipment 구분
+    public SkinType skinType;    // 어떤 외피인지 구분
+}
