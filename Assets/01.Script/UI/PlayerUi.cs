e378601e65c9833176b7eb6fd81a8bc31a3749b0
Park ha <115542242+parkha6@users.noreply.github.com>
2025-12-03 13:10:44 +0900

    fix 플레이어 UI 연동 준비 끝

--- Assets/01.Script/UI/PlayerUi.cs
+++ Assets/01.Script/UI/PlayerUi.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 /// <summary>
@@ -19,17 +20,17 @@ public class PlayerUi : MonoSingleton<PlayerUi>
     /// </summary>
     [SerializeField] Image hpBar;
     /// <summary>
-    /// 외피조각 스코어
-    /// </summary>
-    [SerializeField] Text skinPieceText;
-    /// <summary>
     /// 날개조각 스코어
     /// </summary>
-    [SerializeField] Text wingPieceText;
+    [SerializeField] TMP_Text wingPieceText;
+    /// <summary>
+    /// 외피조각 스코어
+    /// </summary>
+    [SerializeField] TMP_Text skinPieceText;
     /// <summary>
     /// 골드 스코어
     /// </summary>
-    [SerializeField] Text goldPieceText;
+    [SerializeField] TMP_Text goldPieceText;
     [Header("일시정지 UI")]
     /// <summary>
     /// 일시정지 창 닫기 버튼
@@ -38,19 +39,19 @@ public class PlayerUi : MonoSingleton<PlayerUi>
     /// <summary>
     /// 턱 스텟 텍스트
     /// </summary>
-    [SerializeField] Text atkStatText;
+    [SerializeField] TMP_Text atkStatText;
     /// <summary>
     /// 갑각 스텟 텍스트
     /// </summary>
-    [SerializeField] Text skinStatText;
+    [SerializeField] TMP_Text skinStatText;
     /// <summary>
     /// 날개 스텟 텍스트
     /// </summary>
-    [SerializeField] Text wingStatText;
+    [SerializeField] TMP_Text wingStatText;
     /// <summary>
     /// 생명력 스텟 텍스트
     /// </summary>
-    [SerializeField] Text hpStatText;
+    [SerializeField] TMP_Text hpStatText;
     /// <summary>
     /// 일시정지 UI 활성& 비활성
     /// </summary>
@@ -68,6 +69,97 @@ public class PlayerUi : MonoSingleton<PlayerUi>
     {
         if (playerScreenUi != null)
             playerScreenUi.SetActive(isSet);
+    }
+    /// <summary>
+    /// HP를 입력하면 바에 표시 됨.
+    /// </summary>
+    /// <param name="currentHp"></param>
+    /// <param name="wholeHp"></param>
+    internal void SetPlayerHp(float currentHp, float wholeHp)
+    {
+        if (hpBar != null)
+            hpBar.fillAmount = currentHp / wholeHp;
+    }
+    /// <summary>
+    /// 날개,외피,골드 스코어 한번에 세팅
+    /// </summary>
+    /// <param name="wing"></param>
+    /// <param name="skin"></param>
+    /// <param name="gold"></param>
+    internal void SetScoreText(int wing, int skin, int gold)
+    {
+        SetWingScore(wing);
+        SetSkinScore(skin);
+        SetGoldScore(gold);
+    }
+    /// <summary>
+    /// 날개 조각을 스코어에 표시
+    /// </summary>
+    /// <param name="wing"></param>
+    internal void SetWingScore(int wing)
+    {
+        if (wingPieceText != null)
+            wingPieceText.text = wing.ToString();
+    }
+    /// <summary>
+    /// 외피 조각을 스코어에 표시
+    /// </summary>
+    /// <param name="skin"></param>
+    internal void SetSkinScore(int skin)
+    {
+        if (skinPieceText != null)
+            skinPieceText.text = skin.ToString();
+    }
+    /// <summary>
+    /// 골드를 스코어에 표시
+    /// </summary>
+    /// <param name="gold"></param>
+    internal void SetGoldScore(int gold)
+    {
+        if (goldPieceText != null)
+            goldPieceText.text = gold.ToString();
+    }
+    /// <summary>
+    /// 스텟 한번에 세팅
+    /// </summary>
+    internal void SetStat(float atkStat,float skinStat,float wingStat,float hpStat)
+    {
+        SetAtkStat(atkStat);
+        SetSkinStat(skinStat);
+        SetWingStat(wingStat);
+        SetHpStat(hpStat);
+    }
+    /// <summary>
+    /// 턱 스텟 세팅
+    /// </summary>
+    /// <param name="atkStat"></param>
+    internal void SetAtkStat(float atkStat)
+    {
+        if (atkStatText != null)
+            atkStatText.text = $"턱 : {atkStat}%";
+    }
+    /// <summary>
+    /// 갑각 스텟 세팅
+    /// </summary>
+    /// <param name="skinStat"></param>
+    internal void SetSkinStat(float skinStat)
+    {
+        if (skinStatText != null)
+            skinStatText.text = $"갑각 : {skinStat}%";
+    }
+    /// <summary>
+    /// 날개 스텟 세팅
+    /// </summary>
+    /// <param name="wingStat"></param>
+    internal void SetWingStat(float wingStat)
+    {
+        if (wingStatText != null)
+            wingStatText.text = $"날개 : {wingStat}%";
+    }
+    internal void SetHpStat(float hpStat)
+    {
+        if (hpStatText != null)
+            hpStatText.text = $"생명력 : {hpStat}%";
     }
     /// <summary>
     /// 생성시 버튼 연결
